6c624418fac4d968ee612999117498ee56a82462
Wes Turner <50891+westurner@users.noreply.github.com>
2009-04-19 09:08:46 +0000

    Adding course_list test

--- wbcms/tiger/tests.py
+++ wbcms/tiger/tests.py
@@ -1,23 +1,8 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class TigerTest(TestCase):
+    fixtures = ['courses.json']
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+    def test_course_list(self):
+        rsp = self.client.get('/courses/')
+        self.failUnlessEqual(rsp.status_code, 200)
