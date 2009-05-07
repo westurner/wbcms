6500541d577fd405a977a86477ac208b0594520f
Wes Turner <50891+westurner@users.noreply.github.com>
2009-05-07 21:55:17 +0000

    Trimming urls / trailing slashes /

--- wbcms/urls.py
+++ wbcms/urls.py
@@ -13,17 +13,12 @@ urlpatterns = patterns('',
     # Example:
     (r'^users/', include('registration.urls')),
     (r'^accounts/', include('profiles.urls')),
-    
-    (r'^login$', utils.views.login),
+    (r'^login/$', utils.views.login),
     url(r'^logout/$', auth_views.logout,
         {'template_name': 'registration/logout.html'}),
-    
-    (r'^admin/doc/gen/', include('genidef1x.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/(.*)', admin.site.root),
-    (r'^tinymce/', include('tinymce.urls')),
     (r'', include('wbcms.tiger.urls')),
-    (r'', include('cms.urls')),
 )
 
 if settings.DEBUG:
@@ -34,7 +29,7 @@ if settings.DEBUG:
     databrowse.site.register(CourseRequest)
     urlpatterns += patterns('',
         (r'^db/(.*)', login_required(databrowse.site.root)),
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': settings.MEDIA_ROOT}),
+        #(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        #    {'document_root': settings.MEDIA_ROOT}),
 
         )
