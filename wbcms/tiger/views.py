6538de26fabd141e78adf6601d7b0cb7e14bfd6d
Wes Turner <50891+westurner@users.noreply.github.com>
2009-05-07 21:53:51 +0000

    Tiger templates, add'l admin date hierarchy, docstrings

--- wbcms/tiger/views.py
+++ wbcms/tiger/views.py
@@ -15,56 +15,41 @@ from profiles.views import create_profile
 # Courses
 
 def course_list(request):
-    return object_list(request, Course.objects.filter(status__gt=0))
+    """
+    Select and show all active :model:`tiger.Course` objects
+    """
+    return object_list(request, Course.objects.filter(status__gt=0).order_by('subject'))
 
 def course_detail(request, id=None, slug=None):
     """
-    Return a detail page for a course
+    Select and show a :model:`tiger.Course`
     """
     queryset=Course.objects.filter(status__gt=0)
     return object_detail(request, queryset=queryset, slug_field='slug', slug=slug)
 
 # Course Requests
 
-@login_required
 def course_request_create(request,slug):
     """
-    File a course request for a given course
-    
-    
-     1. Login (#4) or Register (#3)
-     2. Create/Update Profile
-     3. Create/Update Course Request
-        - + Course
-        - + Number of Students
-        - + Availability
-        - -> Submit
-        - = Status[-1]: Unverified
-        - Verify Request
-           - -> Modify Request (B.3)
-           - -> Cancel -> Alert(Confirm: yes/no)
-           - -> Submit   
-           - = Status[1] Pending
-        - Thank You + Email Confirm
-           - Course Request
-           - -> Course Catalog
-           - -> Outstanding Course Requests
-
+    File a :model:`tiger.CourseRequest` for a given :model:`tiger.Course`
     """
     
     initial_form_data = {}
 
+    if not request.user.is_authenticated():
+	request.flash["messages"] = [("S Please login or create an account to request this course")]
+        return HttpResponseRedirect('/login/?next=%s' % request.path)
+
     course = lookup_object(Course, None, slug, 'slug')                 
     initial_form_data['course'] = course.id
-
 
     # Lookup profile or redirect to create_profile
     try:
         person = request.user.get_profile()
         initial_form_data['person'] = person.id
     except:
+        request.user.message_set.create(message=_("S Please update your account information"))
         return create_profile(request, success_url=reverse('course_request_create',args=[course.slug]))
-              
 
     
     model, form_class = CourseRequest, CourseRequestForm
@@ -95,20 +80,30 @@ def course_request_create(request,slug):
 
 @login_required
 def course_request_detail(request, id=None, slug=None):
-    queryset=CourseRequest.objects.filter()
+    """
+    Select and show a :model:`tiger.CourseRequest` object
+    """
+    queryset=CourseRequest.objects.select_related()
     return object_detail(request, queryset=queryset, object_id=id)
 
 @login_required
 def course_request_list(request):
+    """
+    Select and show all :model:`tiger.CourseRequest` objects
+    """
     try:
         person = request.user.get_profile()
     except:
         request.user.message_set.create(message=_("S Please update your account information."))
         return create_profile(request, success_url=reverse('course_request_list'))
+        
     return object_list(request, CourseRequest.objects.filter(person=person))
 
 @login_required
 def course_request_update(request):
+    """
+    Update a :model:`tiger.CourseRequest` object
+    """
     return update_object(request, form_class=CourseRequestForm)
 
 
